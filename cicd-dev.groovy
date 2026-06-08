4124fa1bfc8d3be28ffea987c940d46e82a600d6
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2026-06-08 18:53:42 -0400

    Update cicd-dev.groovy
    Signed-off-by: Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -2,11 +2,11 @@ node('linux') {
   stage ('Poll') {
     checkout([
       $class: 'GitSCM', branches: [[name: '*/main']], extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/mbedtls3.6.6port.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/mbedtlsport.git']]])
   }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [
-      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/mbedtls3.6.6port.git'),
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/mbedtlsport.git'),
       string(name: 'PORT_DESCRIPTION', value: 'Mbed TLS is a C library that implements X.509 certificate manipulation and the TLS and DTLS protocols.'),
       string(name: 'BUILD_LINE', value: 'DEV')
     ]
